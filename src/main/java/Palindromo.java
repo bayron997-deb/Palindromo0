f9c4c31303f7d9a16ca14a826b03b641352da886
Bayron <bayron.m.1999@gmail.com>
2020-05-17 19:27:18 -0400

    Se creo una clase llamada Palindromo para ingresar al metodo esPalindromo con sus instrucciones.

--- src/main/java/Palindromo.java
+++ src/main/java/Palindromo.java
@@ -1,2 +1,10 @@
 public class Palindromo {
+    public static boolean esPalindromo(String palabra){
+        /*
+        Se crea un string llamado palabraauxiliar, lo transformamos en stringbuilder y le pasamos "palabra", ya teniendo
+        almacenado la cadena de datos, lo invertimos con un .reverse() y terminamos convirtiendo este stringbuilder en
+        string con un toString().
+         */
+        String palabraauxiliar = new StringBuilder(palabra).reverse().toString();
+    }
 }
