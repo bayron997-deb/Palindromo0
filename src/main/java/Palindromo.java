b5b2366d4c6ec2fb1cba8438cb310168f62e6d09
Bayron <bayron.m.1999@gmail.com>
2020-05-17 19:29:58 -0400

    se agrego un return para devolver un true o false

--- src/main/java/Palindromo.java
+++ src/main/java/Palindromo.java
@@ -1,4 +1,5 @@
 public class Palindromo {
+    
     public static boolean esPalindromo(String palabra){
         /*
         Se crea un string llamado palabraauxiliar, lo transformamos en stringbuilder y le pasamos "palabra", ya teniendo
@@ -6,5 +7,7 @@ public class Palindromo {
         string con un toString().
          */
         String palabraauxiliar = new StringBuilder(palabra).reverse().toString();
+        //creamos un return comparando palabra y palabraauxiliar para que retorne un true o un false.
+        return palabraauxiliar.equals(palabra);
     }
 }
